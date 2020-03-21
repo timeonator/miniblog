788537b5b3774970a7b84209d5e7b27e35f053c2
timeonator <timeonator@gmail.com>
2020-03-21 01:29:06 -0700

    added msg posts literals & view to render them.

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,15 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Roberto'}
-    return render_template('index.html', title='Home', user=user)
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
