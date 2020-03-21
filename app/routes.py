241eefd6996c0038eadfb2151e37baa34da6884a
timeonator <timeonator@gmail.com>
2020-03-21 00:39:46 -0700

    added root app and index template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
-def hello():
-    return 'Hello World'
+def index():
+    user = {'username': 'Roberto'}
+    return render_template('index.html', title='Home', user=user)
