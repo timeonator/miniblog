6bd4f6860d38e8900430b53aaaa0cf97b8b58496
Robert Gifford <timeonator@gmail.com>
2020-03-20 18:49:53 -0700

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+#
+# app package
+from flask import Flask
+
+app = Flask(__name__)
+
+from app import routes
---
+++ app/routes.py
@@ -0,0 +1,6 @@
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def hello():
+    return 'Hello World'
